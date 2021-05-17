7bc834bec646fb6e809c144f6fc395662ba5f4b7
shfosem <37168972+shfosem@users.noreply.github.com>
2021-05-17 23:36:24 +0900

    기본 구성만 해보았습니다.
    일단 잔고정보는 클래스 만들어서, 관리하는게 좋을거 같다고 생각하는데 좀 더 고민해보겠습니다.
    Request form에서는 매수, 매도 결과에 따라서 main에 잔고(balance)를 업데이트할 수 있도록 해야 할 것 같고,
    Chart form은 contextmenustrip시 아이템을 지정해서, api로 차트정보 불러와서 출력하면 좋을 것 같고,
    Search form은 load하면서, api로 코인 정보들 불러오도록 하고, 이름 혹은 종목 번호를 검색하면 상위로 될 수 있게끔 구현하면 될 것 같습니다. search form에도 contextmenustrip등록해 놨는데, main form에서 사용하는 것과 동일하게 구현하면 될 것 같습니다!!

---
+++ CoinCopy/CoinCopy/Chart.Designer.cs
@@ -0,0 +1,85 @@
+﻿
+namespace CoinCopy
+{
+    partial class Chart
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.priceChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.priceChart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // priceChart
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.priceChart.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.priceChart.Legends.Add(legend1);
+            this.priceChart.Location = new System.Drawing.Point(12, 106);
+            this.priceChart.Name = "priceChart";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.priceChart.Series.Add(series1);
+            this.priceChart.Size = new System.Drawing.Size(533, 187);
+            this.priceChart.TabIndex = 0;
+            this.priceChart.Text = "chart1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "종목명";
+            // 
+            // Chart
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(575, 328);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.priceChart);
+            this.Name = "Chart";
+            this.Text = "Chart";
+            ((System.ComponentModel.ISupportInitialize)(this.priceChart)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart priceChart;
+        private System.Windows.Forms.Label label1;
+    }
+}
