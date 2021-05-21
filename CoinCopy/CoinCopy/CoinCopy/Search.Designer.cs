a709c3de2cb71a1ebf5258a612851e9870ca35b3
Sangmin Ju <besforyou2@gmail.com>
2021-05-21 20:36:43 +0900

    coincopy

---
+++ CoinCopy/CoinCopy/CoinCopy/Search.Designer.cs
@@ -0,0 +1,90 @@
+﻿
+namespace CoinCopy
+{
+    partial class Search
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCoinName = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtCoinName
+            // 
+            this.txtCoinName.Location = new System.Drawing.Point(12, 33);
+            this.txtCoinName.Name = "txtCoinName";
+            this.txtCoinName.Size = new System.Drawing.Size(311, 25);
+            this.txtCoinName.TabIndex = 0;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(346, 32);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(80, 26);
+            this.btnSearch.TabIndex = 1;
+            this.btnSearch.Text = "검색";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 83);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 27;
+            this.dataGridView1.Size = new System.Drawing.Size(427, 150);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // Search
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(451, 245);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtCoinName);
+            this.Name = "Search";
+            this.Text = "Search";
+            this.Load += new System.EventHandler(this.Search_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCoinName;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
---
+++ CoinCopy/CoinCopy/CoinCopy/Search.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Net;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+namespace CoinCopy
+{
+    public partial class Search : Form
+    {
+        public Search()
+        {
+            InitializeComponent();
+            try
+            {
+
+                WebClient client = new WebClient();
+                client.Encoding = Encoding.UTF8;
+
+                var reply = client.DownloadString("https://api.upbit.com/v1/market/all");
+
+                DataTable dataTable = (DataTable)JsonConvert.DeserializeObject(reply, (typeof(DataTable)));
+                
+                //"KRW-BTC" +"&count=1"
+                dataTable.Columns.Add("price");
+
+                
+                dataGridView1.DataSource = dataTable;
+                get_price(dataTable);
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+        private void get_price(DataTable dataTable)
+        {
+            string priceurl = "https://api.upbit.com/v1/candles/minutes/1?market=";
+            WebClient tempclient = new WebClient();
+            tempclient.Encoding = Encoding.UTF8;
+            foreach (DataRow row in dataTable.Rows)
+            {
+                string tempurl = priceurl + row["market"].ToString() + "&count=1";
+                var candleinfo = tempclient.DownloadString(tempurl);
+                var price = JsonConvert.DeserializeObject<List<PriceEvent>>(candleinfo);
+                row["price"] = price[0].opening_price.ToString();
+            }
+        }
+
+        private void Search_Load(object sender, EventArgs e)
+        {
+
+        }
+        public class PriceEvent
+        {
+            public string market { get; set; }
+            public DateTime candle_date_time_utc { get; set; }
+            public DateTime candle_date_time_kst { get; set; }
+            public double opening_price { get; set; }
+            public double high_price { get; set; }
+            public double low_price { get; set; }
+            public double trade_price { get; set; }
+            public long timestamp { get; set; }
+            public double candle_acc_trade_price { get; set; }
+            public double candle_acc_trade_volume { get; set; }
+            public int unit { get; set; }
+        }
+    }
+}
