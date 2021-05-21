8aade7683682b4c0aab004f74bcdb694368c53b0
Sangmin Ju <besforyou2@gmail.com>
2021-05-21 21:53:17 +0900

    add balance class
    사용자 계좌 정보를 담는 계좌(balance) 클래스 생성

--- CoinCopy/CoinCopy/CoinCopy/Search.Designer.cs
+++ CoinCopy/CoinCopy/CoinCopy/Search.Designer.cs
@@ -37,16 +37,18 @@ namespace CoinCopy
             // 
             // txtCoinName
             // 
-            this.txtCoinName.Location = new System.Drawing.Point(12, 33);
+            this.txtCoinName.Location = new System.Drawing.Point(10, 26);
+            this.txtCoinName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtCoinName.Name = "txtCoinName";
-            this.txtCoinName.Size = new System.Drawing.Size(311, 25);
+            this.txtCoinName.Size = new System.Drawing.Size(273, 21);
             this.txtCoinName.TabIndex = 0;
             // 
             // btnSearch
             // 
-            this.btnSearch.Location = new System.Drawing.Point(346, 32);
+            this.btnSearch.Location = new System.Drawing.Point(303, 26);
+            this.btnSearch.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnSearch.Name = "btnSearch";
-            this.btnSearch.Size = new System.Drawing.Size(80, 26);
+            this.btnSearch.Size = new System.Drawing.Size(70, 21);
             this.btnSearch.TabIndex = 1;
             this.btnSearch.Text = "검색";
             this.btnSearch.UseVisualStyleBackColor = true;
@@ -57,21 +59,23 @@ namespace CoinCopy
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 83);
+            this.dataGridView1.Location = new System.Drawing.Point(10, 66);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 27;
-            this.dataGridView1.Size = new System.Drawing.Size(427, 150);
+            this.dataGridView1.Size = new System.Drawing.Size(642, 297);
             this.dataGridView1.TabIndex = 2;
             // 
             // Search
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(451, 245);
+            this.ClientSize = new System.Drawing.Size(663, 373);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.btnSearch);
             this.Controls.Add(this.txtCoinName);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Search";
             this.Text = "Search";
             this.Load += new System.EventHandler(this.Search_Load);
